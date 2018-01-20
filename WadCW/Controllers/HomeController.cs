68381f7e85308de4099a3b844ffbce461d2cf520
zarrukhsharipov <zarrukhsharipov@gmail.com>
2018-01-20 23:37:37 +0500

    Commit 2

---
+++ WadCW/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WadCW.Models;
+
+namespace WadCW.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Product = "Smartphone";
+
+            var model = new ProductViewModel()
+            {
+                Name = "Iphone",
+                Price = 9999
+            };
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View(new ProductViewModel());
+        }
+        [HttpPost]
+        public ActionResult AddProduct(ProductViewModel model)
+        {
+            if (ModelState.IsValid)
+                ViewBag.Result = "Product was added successfully";
+            return View(new ProductViewModel());
+        }
+    }
+}
---
+++ WadCW/Models/ProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WadCW.Models
+{
+    public class ProductViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+       [Required]
+       [Range(1,9999999)]
+        public decimal Price { get; set; }
+        [DisplayName("Active")]
+        public bool IsActive { get; set; }
+
+        public string Description { get; set; }
+        [DisplayName("Category")]
+        public Category CategoryId { get; set; }
+    }
+
+    public enum Category
+    {
+        Smartphone,
+        Tablet
+    }
+}
